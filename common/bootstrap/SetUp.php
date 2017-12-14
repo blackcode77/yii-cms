ab6213c410c35d99f4a84716f01912dd20987aa6
BlackCode <eastarodubtsev@ya.ru>
2017-12-14 16:28:08 +0300

    Added AdminLTE

---
+++ common/bootstrap/SetUp.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: blackcode
+ * Date: 13.12.17
+ * Time: 21:54
+ */
+
+namespace common\bootstrap;
+
+
+use yii\base\BootstrapInterface;
+
+class SetUp implements BootstrapInterface
+{
+ public function bootstrap($app)
+ {
+     // TODO: Implement bootstrap() method.
+ }
+}
