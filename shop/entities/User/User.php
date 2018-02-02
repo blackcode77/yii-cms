4b059f59dd242e0f14e8bce2395679fee9c6cf65
BlackCode <eastarodubtsev@ya.ru>
2018-02-02 23:12:35 +0300

    Added user network model

--- shop/entities/User/User.php
+++ shop/entities/User/User.php
@@ -1,9 +1,11 @@
 <?php
 namespace shop\entities\User;
 
+use lhs\Yii2SaveRelationsBehavior\SaveRelationsBehavior;
 use Yii;
 use yii\base\NotSupportedException;
 use yii\behaviors\TimestampBehavior;
+use yii\db\ActiveQuery;
 use yii\db\ActiveRecord;
 use yii\web\IdentityInterface;
 
@@ -21,6 +23,8 @@ use yii\web\IdentityInterface;
  * @property integer $created_at
  * @property integer $updated_at
  * @property string $password write-only password
+ *
+ * @property Network[] $networks
  */
 class User extends ActiveRecord implements IdentityInterface
 {
@@ -40,7 +44,7 @@ class User extends ActiveRecord implements IdentityInterface
         return $user;
     }
 
-    public function confirmSignup()
+    public function confirmSignup(): void
     {
         if (!$this->isWait()) {
             throw new \DomainException('User is already active.');
@@ -49,7 +53,17 @@ class User extends ActiveRecord implements IdentityInterface
         $this->email_confirm_token = null;
     }
 
-    public function requestPasswordReset()
+    public static function signupByNetwork($network, $identity): self
+    {
+        $user = new User();
+        $user->created_at = time();
+        $user->status = self::STATUS_ACTIVE;
+        $user->generateAuthKey();
+        $user->networks = [Network::create($network, $identity)];
+        return $user;
+    }
+
+    public function requestPasswordReset(): void
     {
         if (!empty($this->password_reset_token) && self::isPasswordResetTokenValid($this->password_reset_token)) {
             throw new \DomainException('Password resetting is already requested.');
@@ -57,7 +71,7 @@ class User extends ActiveRecord implements IdentityInterface
         $this->password_reset_token = Yii::$app->security->generateRandomString() . '_' . time();
     }
 
-    public function resetPassword($password)
+    public function resetPassword($password): void
     {
         if (empty($this->password_reset_token)) {
             throw new \DomainException('Password resetting is not requested.');
@@ -76,6 +90,11 @@ class User extends ActiveRecord implements IdentityInterface
         return $this->status === self::STATUS_ACTIVE;
     }
 
+    public function getNetworks(): ActiveQuery
+    {
+        return $this->hasMany(Network::className(), ['user_id' => 'id']);
+    }
+
     /**
      * @inheritdoc
      */
@@ -91,6 +110,17 @@ class User extends ActiveRecord implements IdentityInterface
     {
         return [
             TimestampBehavior::className(),
+            [
+                'class' => SaveRelationsBehavior::className(),
+                'relations' => ['networks'],
+            ],
+        ];
+    }
+
+    public function transactions()
+    {
+        return [
+            self::SCENARIO_DEFAULT => self::OP_ALL,
         ];
     }
 
